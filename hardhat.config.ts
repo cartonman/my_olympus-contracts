e46edcb151374035be02649d6a3cddb29a69e0a1
cartonman <83393764+cartonman@users.noreply.github.com>
2021-12-03 07:14:52 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,76 +1,47 @@
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@nomiclabs/hardhat-etherscan";
-
-import { resolve } from "path";
-
-import { config as dotenvConfig } from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import { NetworkUserConfig } from "hardhat/types";
 
 const { privateKey } = require('./secrets.json');
 
-const config: HardhatUserConfig = {
-  defaultNetwork: "localhost",
-  gasReporter: {
-    currency: "USD",
-    enabled: process.env.REPORT_GAS ? true : false,
-    excludeContracts: [],
-    src: "./contracts",
-  },
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    },
-    hardhat: {
-    },
-    rinkeby: {
-      url: "http://127.0.0.1:8545"
-    },
-    fantomtestnet: {
-      url: "https://rpc.testnet.fantom.network/",
-      chainID : 4002,
-      accounts: [privateKey]
-    }
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    compilers: [
-      {
-        version: "0.7.5",
-        settings: {
-          metadata: {
-            bytecodeHash: "none",
-          },
-          optimizer: {
-            enabled: true,
-            runs: 800,
-          },
-        },
+module.exports = {
+    defaultNetwork: "localhost",
+    networks: {
+      localhost: {
+        url: "http://127.0.0.1:8545"
       },
-      {
-        version: "0.5.16",
+      hardhat: {
       },
-    ],
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["storageLayout"]
-        }
+      rinkeby: {
+      	url: "http://127.0.0.1:8545"
+      },
+      fantomtestnet: {
+      	url: "https://rpc.testnet.fantom.network/",
+      	chainID : 4002,
+      	  accounts: [privateKey]
       }
-    }
-  },
-  typechain: {
-    outDir: "types",
-    target: "ethers-v5",
-  }
+    },
+    solidity: {
+	compilers: [
+	    {
+		version: "0.7.5",
+		settings:
+		{
+		    optimizer:
+		    {
+			enabled: true,
+			runs: 200
+		    },
+		},
+	    },
+	    {
+		version: "0.8.0",
+		settings:
+		{
+		    optimizer: {
+			enabled: true,
+			runs: 200,
+		    },
+		},
+	    },
+	],
+    },
 };
-
-export default config;
